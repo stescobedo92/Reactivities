b1ed1710a1a607a47c47a11b269805e9684b927b
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-09-16 11:58:03 -0600

    Add DataContext, remove Class1, update EF Core SQLite
    Removed the empty Class1 class and its namespace from Class1.cs.
    Updated Persistence.csproj to include Microsoft.EntityFrameworkCore.Sqlite version 8.0.8.
    Added DataContext.cs with DataContext class inheriting from DbContext, including a DbSet<Activity> property named Activities.

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Activity> Activities { get; set; }
+}
