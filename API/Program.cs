ca8083e480f5bf67d9f12fdc60d9796717273a27
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-09-16 12:03:34 -0600

    Configure SQLite database for API project
    Added a project reference to `Persistence.csproj` in `API.csproj`.
    Included `Microsoft.EntityFrameworkCore` and `Persistence` namespaces in `Program.cs`.
    Configured the `DataContext` to use SQLite with a connection string from the configuration in `Program.cs`.
    Added a connection string for the SQLite database to `appsettings.Development.json`.

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DataBaseDefaultConnection"));
+});
 
 var app = builder.Build();
 
