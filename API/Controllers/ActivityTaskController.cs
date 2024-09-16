c6b985c118526dde4094493f2579d25fe0283c78
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-09-16 14:08:51 -0600

    Add ActivityTaskController and BaseApiController
    Introduce ActivityTaskController to handle HTTP requests for ActivityTask entities, including methods to get all activities and a specific activity by ID. Add BaseApiController as a base class for API controllers with common configuration and attributes.

---
+++ API/Controllers/ActivityTaskController.cs
@@ -0,0 +1,34 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers;
+
+public class ActivityTaskController : BaseApiController
+{
+    private readonly DataContext _context;
+
+    public ActivityTaskController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet] //api/activitytask
+    public async Task<ActionResult<List<ActivityTask>>> GetActivities()
+    {
+        return await _context.Activities.ToListAsync();
+    }
+
+    [HttpGet("{id}")] //api/activitytask/{id}
+    public async Task<ActionResult<ActivityTask>> GetActivity(Guid id)
+    {
+        var activity = await _context.Activities.FindAsync(id);
+
+        if (activity == null)
+        {
+            return NotFound();
+        }
+        return activity;
+    }
+}
