8f4b9149cb4c6a1fec9775ea4ad09f228858db96
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-10-22 15:46:41 -0600

    Deleting activity on the server

--- client-app/src/features/activities/dashboard/AxctivityList.tsx
+++ client-app/src/features/activities/dashboard/AxctivityList.tsx
@@ -1,13 +1,22 @@
 import {Activity} from "../../../app/models/activity.ts";
 import {Button, Item, Label, Segment} from "semantic-ui-react";
+import {SyntheticEvent, useState} from "react";
 
 interface Props {
     activities: Activity[];
     selectActivity: (id: string) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
-export default function ActivityList({activities, selectActivity, deleteActivity}: Props) {
+export default function ActivityList({activities, selectActivity, deleteActivity, submitting}: Props) {
+  const [target, setTarget] = useState('');
+
+  function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
+    setTarget(e.currentTarget.name);
+    deleteActivity(id);
+  }
+
   return (
     <Segment>
         <Item.Group divided>
@@ -22,7 +31,14 @@ export default function ActivityList({activities, selectActivity, deleteActivity
                         </Item.Description>
                         <Item.Extra>
                             <Button onClick={ () => selectActivity(activity.id)} floated={"right"} content={"View"} color={"blue"} />
-                            <Button onClick={ () => deleteActivity(activity.id)} floated={"right"} content={"Delete"} color={"red"} />
+                            <Button
+                                name={activity.id}
+                                loading={submitting && target === activity.id}
+                                onClick={ (e) => handleActivityDelete(e, activity.id)}
+                                floated={"right"}
+                                content={"Delete"}
+                                color={"red"}
+                            />
                             <Label basic content={activity.category} />ß
                         </Item.Extra>
                     </Item.Content>
