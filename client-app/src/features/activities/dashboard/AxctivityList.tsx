e3739b232f5d5340d9a72b99f6fcf20e169bd6ff
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-10-22 14:03:46 -0600

    Deleting an activity

--- client-app/src/features/activities/dashboard/AxctivityList.tsx
+++ client-app/src/features/activities/dashboard/AxctivityList.tsx
@@ -4,9 +4,10 @@ import {Button, Item, Label, Segment} from "semantic-ui-react";
 interface Props {
     activities: Activity[];
     selectActivity: (id: string) => void;
+    deleteActivity: (id: string) => void;
 }
 
-export default function ActivityList({activities, selectActivity}: Props) {
+export default function ActivityList({activities, selectActivity, deleteActivity}: Props) {
   return (
     <Segment>
         <Item.Group divided>
@@ -21,6 +22,7 @@ export default function ActivityList({activities, selectActivity}: Props) {
                         </Item.Description>
                         <Item.Extra>
                             <Button onClick={ () => selectActivity(activity.id)} floated={"right"} content={"View"} color={"blue"} />
+                            <Button onClick={ () => deleteActivity(activity.id)} floated={"right"} content={"Delete"} color={"red"} />
                             <Label basic content={activity.category} />ß
                         </Item.Extra>
                     </Item.Content>
