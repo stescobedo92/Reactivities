410c26a115eb82d8b1503724874dcc0d5340fd46
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-10-22 14:31:29 -0600

    Setting up axios

---
+++ client-app/src/app/api/agent.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import {Activity} from "../models/activity.ts";
+
+axios.defaults.baseURL = 'http://localhost:5104/api';
+
+const responseBody = <T>(response: any) => response.data;   // This is a generic function that takes a response and returns the data property of the response.
+
+const requests = {
+    get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+    post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+    put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+    del: <T>(url: string) => axios.delete<T>(url).then(responseBody)
+};
+
+const Activities = {
+    list: () => requests.get<Activity[]>('/ActivityTask'),
+    details: (id: string) => requests.get<Activity>(`/ActivityTask/${id}`),
+    create: (activity: Activity) => requests.post<void>('/ActivityTask', activity),
+    update: (activity: Activity) => requests.put<void>(`/ActivityTask/${activity.id}`, activity),
+    delete: (id: string) => requests.del<void>(`/ActivityTask/${id}`)
+};
+
+const agent = {
+    Activities
+}
+
+export default agent;
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
 import {useEffect, useState} from "react";
-import axios from "axios";
 import {Container} from "semantic-ui-react";
 import {Activity} from "../models/activity.ts";
 import NavBar from "./NavBar.tsx";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard.tsx";
 import {v4 as uuid} from "uuid";
+import agent from "../api/agent.ts";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -12,10 +12,8 @@ function App() {
   const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5104/api/ActivityTask')
-        .then(response => {
-          console.log(response);
-          setActivities(response.data);
+    agent.Activities.list().then(response => {
+          setActivities(response);
         });
   }, []);
 
