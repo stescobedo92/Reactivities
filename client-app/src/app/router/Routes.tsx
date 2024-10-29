d5ee2fb75650f6b050128000f324eb811ccda5de
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-10-29 11:16:36 -0600

    Moving the home page outside of nav

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,6 +1,5 @@
 import {createBrowserRouter, RouteObject} from "react-router-dom";
 import App from "../layout/App.tsx";
-import HomePage from "../../features/home/HomePage.tsx";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard.tsx";
 import ActivityForm from "../../features/activities/form/ActivityForm.tsx";
 import ActivityDetails from "../../features/activities/details/ActivityDetails.tsx";
@@ -10,7 +9,6 @@ export const routes: RouteObject[] =[
         path: '/',
         element: <App />,
         children: [
-            {path: '', element: <HomePage />},
             {path: 'activities', element: <ActivityDashboard />},
             {path: 'activities/:id', element: <ActivityDetails />},
             {path: 'createActivity', element: <ActivityForm key='create' />},
